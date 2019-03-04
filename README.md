17a74bb59667c0c0cc9098d9f845270011f5775d
Tyler Swindell <electromagnetic.jolt@gmail.com>
2019-03-04 05:15:55 -0500

    first build

---
+++ README.md
@@ -0,0 +1,7 @@
+# news
+
+A new Flutter project aimed at delivering news.
+
+## News
+
+This is an app I'm working on in a Udemy course to learn Flutter.
---
+++ test/src/resources/news_api_provider_test.dart
@@ -0,0 +1,11 @@
+import 'package:news/src/resources/news_api_provider.dart';
+import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+
+void main() {
+test('FetchTopIds returns a list of ids', (){
+  final newsApi =NewsApiProvider();
+});
+}
